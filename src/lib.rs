e8249000e50c82bb7bc4b99afb3aad3c2db5f281
dj8yf0μl <feepdake27@gmail.com>
2024-07-02 23:26:32 +0300

    next

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
-use near_sdk::{log, near};
+use near_sdk::near;
 
 #[near(contract_state)]
 pub struct Contract {
-    greetings: near_sdk::store::Vector<String>,
+    greetings: near_sdk::store::IterableSet<String>,
 }
 
 impl Default for Contract {
     fn default() -> Self {
         Self {
-            greetings: near_sdk::store::Vector::new(b"g"),
+            greetings: near_sdk::store::IterableSet::new(b"g"),
         }
     }
 }
 
 #[near]
 impl Contract {
-    pub fn get_greeting(&self) -> String {
-        self.greetings.get(1).unwrap().clone()
+    pub fn get_greeting(&self) -> bool {
+        self.greetings.contains("host")
     }
 }
